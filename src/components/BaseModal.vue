da045e8d33cf111b35d80b96d9d7fe7b1bbc1bc0
Rafsan <rafsan.quayes@gmail.com>
2023-07-12 19:21:23 +0600

    Added transition animation for modal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,10 +1,14 @@
 <template>
-    <div v-show="modalActive" class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-md">
-            <slot />
-            <button @click="$emit('close-modal')" class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+    <Transition name="modal-outer">
+        <div v-show="modalActive" class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+            <Transition name="modal-inner">
+                <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-md">
+                    <slot />
+                    <button @click="$emit('close-modal')" class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+                </div>
+            </Transition>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <script setup>
@@ -16,4 +20,14 @@ defineProps({
     }
 })
 </script>
+
+<style scoped>
+.modal-outer-enter-active, .modal-outer-leave-active{
+    transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from, .modal-outer-leave-to{
+    opacity: 0;
+}
+</style>
 
