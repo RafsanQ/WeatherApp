314f7e935e9f3b870ea774946997917359461c88
Rafsan <rafsan.quayes@gmail.com>
2023-07-12 18:59:00 +0600

    Created the info modal

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -12,10 +12,33 @@
                 <i class="fa-solid fa-circle-info hover:text-weather-secondary duration-150 cursor-pointer"></i>
                 <i class="fa-solid fa-plus hover:text-weather-secondary duration-150 cursor-pointer"></i>
             </div>
+            <BaseModal>
+                <div class="text-black">
+                    <h1 class="text-2xl mb-1">About:</h1>
+                    <p class="mb-4">
+                        The Local Weather page allows you to track the current and future weather of cities of your choosing.
+                    </p>
+
+                    <h2 class="text-xl">How it works:</h2>
+                    <ol class="list-decimal list-inside mb-4">
+                        <li>Search for your city by entering the name of your desired city into the search bar</li>
+
+                        <li>Select a city from the results. This will take you to the current weather of that city.</li>
+
+                        <li>Track the city by clicking on the '+' icon on the top right. This will save the city onto the homepage to view at a later time.</li>
+                    </ol>
+
+                    <h2 class="text-xl">Removing a city:</h2>
+                    <p class="mv-4">
+                        If you no longer wish to track a city, simply select it from the homepage and click on the delete option that will appear at the bottom of the page.
+                    </p>
+                </div>
+            </BaseModal>
         </nav>
     </header>
 </template>
 
 <script setup>
     import { RouterLink } from 'vue-router';
+    import BaseModal from './BaseModal.vue';
 </script>
